2c5b9b1f7519c454759da2ebe8c5ae5f1c7e5e3a
hTKzmak <ineedrtx@gmail.com>
2023-12-26 13:30:09 +0300

    price update (26.11.2023)

--- frontend/src/components/OrderDetails/index.jsx
+++ frontend/src/components/OrderDetails/index.jsx
@@ -7,7 +7,8 @@ function OrderDetails({ setActivate }) {
     const basketValue = localStorage.getItem('basket');
     const basketObj = JSON.parse(basketValue);
     const totalCount = basketObj.Basket.reduce((acc, item) => acc + item.count, 0);
-    const totalPrice = basketObj.Basket.reduce((acc, item) => acc + (item.discont_price == 'null' ? item.price : item.discont_price), 0);
+    const totalPrice = basketObj.Basket.reduce((acc, item) => acc + ((item.discont_price !== null) ? item.discont_price : item.price), 0);
+    
 
     return (
         <div className={style.orderForm}>
@@ -16,7 +17,7 @@ function OrderDetails({ setActivate }) {
                 <h2>{totalCount} Items</h2>
                 <div className={style.totalPrice}>
                     <h2>Total</h2>
-                    <h1>${totalPrice * totalCount}</h1>
+                    <h1>${Math.round(totalPrice * totalCount)}</h1>
                 </div>
             </div>
             <div className={style.form}>
